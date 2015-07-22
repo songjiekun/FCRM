d10cb1c4f82184efa5aaceef8aef5198e1fa0250
song jie kun <songjiekun@gmail.com>
2015-07-22 17:50:41 +0800

    Initial Commit

---
+++ FCRM/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  FCRM
+//
+//  Created by song jiekun on 15/7/22.
+//  Copyright (c) 2015年 song jiekun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ FCRM/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FCRM
+//
+//  Created by song jiekun on 15/7/22.
+//  Copyright (c) 2015年 song jiekun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
